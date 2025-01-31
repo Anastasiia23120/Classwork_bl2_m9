3d3ac1a98dc2913908f8c948822a7962230ae480
Anastasiia Medved <nastya0662793393@gmail.com>
2025-01-31 01:15:27 +0100

    last part

--- js/02-localstorage.js
+++ js/02-localstorage.js
@@ -1,1 +1,30 @@
+// const product = {
+//   title: 'Some title',
+//   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, earum.',
+//   price: 1000,
+// };
+
+// localStorage.setItem('test', 'some string');
+// localStorage.setItem('product-info', JSON.stringify(product));
 //*----------------------------------------------------------
+// console.log(localStorage.getItem('test'));
+
+// const product = JSON.parse(localStorage.getItem('product-info'));
+// // const product = JSON.parse(localStorage.getItem('product-i'));
+
+// console.log(product);
+//*----------------------------------------------------------
+// localStorage.removeItem('product-info');
+// localStorage.clear();
+//*----------------------------------------------------------
+// const product = {
+//   title: 'Some title',
+//   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, earum.',
+//   price: 1000,
+// };
+
+// sessionStorage.setItem('test', 'some string');
+// sessionStorage.setItem('product-info', JSON.stringify(product));
+
+// sessionStorage.removeItem('product-info');
+// sessionStorage.clear();
